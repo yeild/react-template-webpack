c1451f547ea321278eeab1892345f417bd4ed65b
huanglei <798400626@qq.com>
2018-10-29 09:29:19 +0800

    update generated info

--- utils/index.js
+++ utils/index.js
@@ -18,7 +18,7 @@ exports.sortDependencies = function sortDependencies(data) {
 exports.printMessage = function printMessage(data) {
   console.log('---------------')
   console.log('To get started:')
-  console.log('  cd', data.name)
+  console.log(data)
   console.log('  npm install')
   console.log('  npm run dev')
 }
